d739c66d53d24e832cb99c56bcc1f32960ac2732
Jorge Smulevici <Jsmulevici@Jsmulevici-MBK13.local>
2019-03-14 17:13:39 +0200

    maker header processor optional and fix tests paths

--- test/header_processors/generic_test.rb
+++ test/header_processors/generic_test.rb
@@ -1,4 +1,4 @@
-require './test/test_helper'
+require_relative '../test_helper'
 
 module CsvComposer::HeaderProcessors
   class GenericCsvHeaderProcessorTest < Minitest::Test
--- test/writer_test.rb
+++ test/writer_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require_relative 'test_helper'
 
 module CsvComposer
   class WriterTest < Minitest::Test
@@ -20,6 +20,7 @@ module CsvComposer
       header_processor = Minitest::Mock.new
       headers = %w(header1 header2)
       header_processor.expect(:process, headers, [@mapping, {}])
+      header_processor.expect(:nil?, false)
 
       item_processor = Minitest::Mock.new
       processed_first_item = [@items[0][:name], @items[0][:value]]
@@ -36,5 +37,19 @@ module CsvComposer
       assert_mock(item_processor)
     end
 
-  end
+    def test_generates_the_right_contect_when_header_processor_is_not_present
+      item_processor = Minitest::Mock.new
+      processed_first_item = [@items[0][:name], @items[0][:value]]
+      processed_last_item = [@items[1][:name], @items[1][:value]]
+
+      item_processor.expect(:process_value, processed_first_item, [@items[0], @mapping, { headers: headers }])
+      item_processor.expect(:process_value, processed_last_item, [@items[1], @mapping, { headers: headers }])
+
+      content = @writer.write(nil, item_processor, @items, @mapping)
+      expected_content = "#{@items[0][:name]},#{@items[0][:value]}\n#{@items[1][:name]},#{@items[1][:value]}\n"
+
+      assert_equal(expected_content, content)
+      assert_mock(item_processor)
+    end
+end
 end
